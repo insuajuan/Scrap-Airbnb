7365b66dbdfd5a68b264f7be19f61a6e38eace0c
Juan I <juan_insua88@hotmail.com>
2022-07-03 21:37:53 -0300

    completed whole process

---
+++ helpers.py
@@ -0,0 +1,133 @@
+import time
+
+from bs4 import BeautifulSoup
+import requests
+from selenium import webdriver
+from selenium.webdriver.safari.options import Options
+
+import openpyxl
+
+from tqdm import tqdm
+
+
+def scrape_page(page_url):
+
+    resp = requests.get(page_url)
+    content = resp.content
+    soup = BeautifulSoup(content, features='html.parser')
+
+    return soup
+
+
+def get_all_href(page_url):
+    href_list = list()
+    html_content = scrape_page(page_url)
+    for link in html_content.find_all('a'):
+        href = link.get('href')
+        href_list.append(href)
+    return href_list
+
+
+def listing_pages(url, listings_per_page=20, pages_per_location=15):
+
+    url_list = []
+    for i in range(pages_per_location):
+        offset = listings_per_page * i
+        url_pagination = url + f'&items_offset={offset}'
+        url_list.append(url_pagination)
+
+    return url_list
+
+
+def get_all_rooms_url(page_url):
+    url_pages_list = listing_pages(page_url)
+
+    rooms_list = list()
+    for page in tqdm(url_pages_list):
+        href_list = get_all_href(page)
+        for i in range(len(href_list) - 1):
+            if '/rooms' in href_list[i] and href_list[i - 1] != href_list[i]:
+                rooms_list.append(href_list[i])
+
+    return rooms_list
+
+
+def get_property_info(url):
+    # Create Selenium driver
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+
+    driver = webdriver.Chrome(options=options)
+    # get price (need to use Selenium as there is JS that takes a second to load)
+    driver.get(url)
+    time.sleep(3)
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'html.parser')
+
+    price_elem = soup.find(class_="_1k4xcdh")
+    # response: $83.720 ARS
+    price = price_elem.get_text()
+
+    property_name_elem = soup.find('h1')
+    property_name = property_name_elem.get_text()
+
+    driver.close()
+
+    return property_name, price
+
+
+def create_excel(city, country, checkin, checkout, adults, children):
+    workbook = openpyxl.Workbook()
+    sheet = workbook.active
+    sheet.title = "Results"
+
+    b2 = sheet['B2']
+    b2.value = "Location"
+    c2 = sheet['C2']
+    c2.value = f"{city} - {country}"
+
+    b3 = sheet['B3']
+    b3.value = "Check In"
+    c3 = sheet['C3']
+    c3.value = checkin
+
+    b4 = sheet['B4']
+    b4.value = "Check Out"
+    c4 = sheet['C4']
+    c4.value = checkout
+
+    b5 = sheet['B5']
+    b5.value = "Adults"
+    c5 = sheet['C5']
+    c5.value = adults
+
+    b6 = sheet['B6']
+    b6.value = "Children"
+    c6 = sheet['C6']
+    c6.value = children
+
+    b8 = sheet['B8']
+    b8.value = "Property"
+    c8 = sheet['C8']
+    c8.value = "Price"
+    d8 = sheet['D8']
+    d8.value = "Link"
+
+    return workbook
+
+
+def store_to_excel(sheet, prop_dict):
+    # i=9 because we start writing excel data in cell row 9
+    i = 9
+    for key in prop_dict:
+        # B column is for property name
+        sheet[f"B{i}"].value = key
+        # C column is for price
+        price = prop_dict[key][0]
+        sheet[f"C{i}"].value = price
+        # D column is for link
+        link = prop_dict[key][1]
+        sheet[f"D{i}"].value = link
+
+        i += 1
--- main.py
+++ main.py
@@ -1,98 +1,51 @@
-import time
-
-from bs4 import BeautifulSoup
-import requests
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.safari.options import Options
-
-
-def scrape_page(page_url):
-
-    resp = requests.get(page_url)
-    content = resp.content
-    soup = BeautifulSoup(content, features='html.parser')
-
-    return soup
-
-
-def get_all_href(page_url):
-    href_list = list()
-    html_content = scrape_page(page_url)
-    for link in html_content.find_all('a'):
-        href = link.get('href')
-        href_list.append(href)
-    return href_list
-
-
-def get_rooms_url(page_url):
-    rooms_list = list()
-    href_list = get_all_href(page_url)
-    for link in href_list:
-        if '/rooms' in link:
-            rooms_list.append(link)
-
-    return rooms_list
-
-
-def listing_pages(url, listings_per_page=20, pages_per_location=15):
-
-    url_list = []
-    for i in range(pages_per_location):
-        offset = listings_per_page * i
-        url_pagination = url + f'&items_offset={offset}'
-        url_list.append(url_pagination)
-
-    return url_list
-
-
-def get_all_rooms_url(page_url):
-    pages_to_scrap = listing_pages(page_url)
-    rooms_list = list()
-    for page in pages_to_scrap:
-        rooms = get_rooms_url(page)
-        rooms_list.append(rooms)
-
-    return rooms_list
-
+from helpers import create_excel, get_all_rooms_url, get_property_info, store_to_excel
+from tqdm import tqdm
 
 # prompt user for city, country, people, check-in, check-out
-city = input('please provide city name. i.e: Buenos Aires\n')
-city.replace(" ", "-")
-country = input('please provide country name. i.e: Argentina\n')
-country.replace(" ", "-")
-adults = input('adults:\n')
-checkin = input('checkin? YYYY-MM-DD\n')
-checkout = input('checkout? YYYY-MM-DD\n')
+# TO-DO: wrap around try except block. Validate user input
+city = input('Please provide city name. i.e: Buenos Aires\n')
+if city == "testing":
+    city = 'Buenos Aires'
+    country = "Argentina"
+    checkin = "2022-10-20"
+    checkout = "2022-11-20"
+    adults = 1
+    children = 0
 
+else:
+    city.replace(" ", "-")
+    country = input('Please provide country name. i.e: Argentina\n')
+    country.replace(" ", "-")
+    adults = input('adults:\n') # min 1 max 16
+    children = input('children:\n') # min 0 max 6
+    checkin = input('checkin? YYYY-MM-DD\n')
+    checkout = input('checkout? YYYY-MM-DD\n') # validate that is > checkin
+
+
+# Create Excel Workbook
+wb = create_excel(city, country, checkin, checkout, adults, children)
+result_sheet = wb.active
+
+# Main search URL
 homepage = 'http://www.airbnb.com'
-main_listing = f'{homepage}/s/{city}--{country}/homes?adults={adults}&checkin={checkin}&checkout={checkout}'
+main_listing = f'{homepage}/s/{city}--{country}/homes?' \
+               f'adults={adults}&children={children}&checkin={checkin}&checkout={checkout}'
+
 
 # get links for all properties. Returns list of lists
-# all_rooms_links = get_all_rooms_url(main_listing)
-# iterate to open each link
+print('**********************************')
+print("Getting all urls to search for final prices")
 
-example_property = 'https://www.airbnb.com.ar/rooms/41650674?adults=1&check_in=2022-10-10&check_out=2022-11-11&display_extensions%5B%5D=MONTHLY_STAYS&federated_search_id=265c03ad-7421-4d0c-8971-63e0b351da69&source_impression_id=p3_1656822314_80w88Md%2BQKxEEIdO'
+all_rooms_links = get_all_rooms_url(main_listing)
 
-# get price (need to use Selenium as there is JS that takes a second to load)
-soup_property = scrape_page(example_property)
-options = Options()
-options.add_argument('--headless')
-options.add_argument('--no-sandbox')
-driver = webdriver.Safari(options=options)
-
-driver.get(example_property)
-time.sleep(2)
-html = driver.page_source
-soup = BeautifulSoup(html, 'html.parser')
-price = soup.find(class_="_1k4xcdh")
-# response: $83.720 ARS
-print(price.get_text())
-
-
-property_name = soup.find('h1')
-print(property_name.get_text())
-
-
-driver.close()
+# iterate to open each link and save info to a dictionary
+properties_dict = dict()
+for link in tqdm(all_rooms_links):
+    property_name, price = get_property_info(homepage+link)
+    properties_dict[property_name] = [price, homepage+link]
+#
+# # iterate over the property dictionary to write info to excel
+store_to_excel(result_sheet, properties_dict)
+#
+# # Save workbook and close driver
+wb.save('Airbnb-results.xlsx')
